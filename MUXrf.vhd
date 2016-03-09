fe69cb00f70e4f47003a1a098ff3b070efd20e7b
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-08 22:28:33 -0500

    Update MUXrf.vhd
    creación de contenido de arquitectura!

--- MUXrf.vhd
+++ MUXrf.vhd
@@ -1,11 +1,19 @@
 entity MUXrf is
 	port(
+	RegDst: in std_logic;
 	ent1: in STD_LOGIC_VECTOR (4 downto 0);
 	ent2: in STD_LOGIC_VECTOR (4 downto 0);
-	sal_ out STD_LOGIC_VECTOR (4 downto 0)
+	sal: out STD_LOGIC_VECTOR (4 downto 0)
 	);
 end MUXrf;
 
 architecture MUXrf_arch of MUXrf is
-
+process (RegDst)
+	begin
+		if (RegDst = "1") then
+			sal <= ent2;
+		else then
+			sal <= ent1;
+		end if;
+	end process;
 end MUXrf_arch;
