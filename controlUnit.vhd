3c272cb3bb8f53098e69e963d13f371831ee6767
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-10 11:02:24 -0500

    cambio de if a when,else.
    Revisar que es más optimo. Que puede causar menos comportamientos erraticos en ejecución.

--- controlUnit.vhd
+++ controlUnit.vhd
@@ -1,10 +1,19 @@
+
+
 entity controlUnit is
 	port(
 	op: in STD_LOGIC_VECTOR (5 downto 0);
-	clk: in std_logic;
-	datapath: STD_LOGIC_VECTOR(11 downto 0));
+	datapath: STD_LOGIC_VECTOR(12 downto 0));
 end controlUnit;
 
 architecture CU_arch of controlUnit is
+	process (op)
+	begin
+			datapath <= "0001000110000" when op="000000"else --tipo R
+			datapath <= "0010001110000" when op="001000"else --addi
+			datapath <= "0011000000110" when op="000100"else --beq
+			datapath <= "0011000000010" when op="000101"else --bne
+			datapath <= "0010000110100" when op="000011"else --jal
+	end process;
 
 end CU_arch
