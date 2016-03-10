780bf1f5ae26c6d4b900e09713991e58a76809e5
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-09 20:31:48 -0500

    Arquitectura de shiftleft2
    revisar ieee.numeric_std!!

--- shiftleft2.vhd
+++ shiftleft2.vhd
@@ -1,3 +1,5 @@
+ieee.numeric_std
+
 entity shiftleft2 is
 	port(
 	ent: in STD_LOGIC_VECTOR (31 downto 0);
@@ -6,5 +8,6 @@ entity shiftleft2 is
 end shiftleft2;
 
 architecture shiftleft2_arch of shiftleft2 is
+	sal <= ent sll 2;
 
 end shiftleft2;
