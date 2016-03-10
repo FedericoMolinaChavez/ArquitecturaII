d1c24c6cca0bf291aa5260da4ec2c9af51eb6fd7
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-10 12:13:52 -0500

    comienso de arquitectura de ALU

--- ALU.vhd
+++ ALU.vhd
@@ -9,5 +9,10 @@ entity ALU is
 end ALU;
 
 architecture ALU_arch of ALU is
+	process (ALUop,entA,entB,res1)
+	begin
+		res1<=std_logic_vector(unsigned(entA) + unsigned(entB)) when Aluop="0010"else
+		
+	end process
 
 end ALU_arch
