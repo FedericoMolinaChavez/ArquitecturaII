377abd40d9bd6a19f7711d944f3df49d87ca8258
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-11 23:04:57 -0500

    version definitiva de MUXaddpc
    Se replicara funcionamiento a resto de MUXES del sistema.

--- MUXaddpc.vhd
+++ MUXaddpc.vhd
@@ -6,18 +6,20 @@ entity MUXaddpc is
   ent1: in STD_LOGIC_VECTOR (31 downto 0);
   ent2: in STD_LOGIC_VECTOR (31 downto 0);
   ReslB: in std_logic;
-  sal: buffer STD_LOGIC_VECTOR (31 downto 0)
+  sal: out STD_LOGIC_VECTOR (31 downto 0)
   );
 end MUXaddpc;
 
 architecture MUXaddpc_arch of MUXaddpc is
+  signal sal_dummy : STD_LOGIC_VECTOR(31 downto 0);
     begin
+    sal <= sal_dummy;
     process(ent1,ent2,ReslB,sal)
     begin
       case ReslB is
-        when '1' => sal<=ent1;
-        when '0' => sal<=ent2;
-        when others => sal<=ent1;
+        when '1' => sal_dummy<=ent1;
+        when '0' => sal_dummy<=ent2;
+        when others => sal_dummy<=ent1;
       end case; 
     end process;
 end MUXaddpc_arch;
