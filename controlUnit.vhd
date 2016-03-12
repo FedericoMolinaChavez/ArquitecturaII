da86f7f547aa8c6d49631abf9a48f7a2f4492e80
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-12 12:08:43 -0500

    cambios estructura concurrente

--- controlUnit.vhd
+++ controlUnit.vhd
@@ -4,18 +4,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity controlUnit is
 	port(
 	op: in STD_LOGIC_VECTOR (5 downto 0);
-	datapath: STD_LOGIC_VECTOR(12 downto 0));
+	datapath: out STD_LOGIC_VECTOR(12 downto 0));
 end controlUnit;
 
 architecture CU_arch of controlUnit is
+signal datapath_dummy: STD_LOGIC_VECTOR(12 downto 0);
 begin
-	process (op) begin
-			datapath <= "0001000110000" when (op="000000") else --tipo R
-			datapath <= "0010001110000" when (op="001000") else --addi
-			datapath <= "0011000000110" when (op="000100") else --beq
-			datapath <= "0011000000010" when (op="000101") else --bne
-			datapath <= "0010000110100" when (op="000011") else --jal
-			datapath <= "0000000000000";
+	datapath<=datapath_dummy;
+	process (op, datapath_dummy) begin
+		case op is
+			when "000000" => datapath_dummy <= "0001000110000";
+			when "001000" => datapath_dummy <= "0010001110000";
+			when "000100" => datapath_dummy <= "0011000000110";
+			when "000101" => datapath_dummy <= "0011000000010";
+			when "000011" => datapath_dummy <= "0010000110100";
+			when others => datapath_dummy <= "0000000000000";
+		end case;
 	end process;
 
 end CU_arch;
+
