06d507393d88a883429f37dfe7f8e5dfdc479ecc
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-12 11:51:20 -0500

    cambios en estructura when, falta agregar algunas opciones!

--- CA.vhd
+++ CA.vhd
@@ -11,27 +11,28 @@ entity CA is
 end CA;
 
 architecture CA_arch of CA is
+  signal sal_dummy: STD_LOGIC_VECTOR (3 downto 0);
 begin
-  process(op,funct) 
+  sal <= sal_dummy;
+  process(op,funct,AluOp,sal_dummy) 
     begin
       if (op = "000000") then --Si es tipo R
-        sal<="0010" when (funct="100000")else
-        sal<="0110" when (funct="100010") else
-        sal<="1110" when (funct="011010")else
-        sal<="0111" when (funct="101010")else
-        sal<="1111" when (funct="010000")else
-        sal<="0000";
-        
-      else then
-        sal<="0010" when op="001000"else --addi
-        sal<="0110" when op="000100"else --beq
-        sal<="0110" when op="000101"else --bne
-        sal<="1000" when op="000011"else --jal
-		  sal<="0000";
-      endif;
-      
-      
-        
+      case funct is
+        when "100000" => sal_dummy<="0010";
+        when "100010" => sal_dummy<="0110";
+        when "011010" => sal_dummy<="1110";
+        when "101010" => sal_dummy<="0111";
+        when "010000" => sal_dummy<="1111";
+        when others => sal_dummy<="0000";
+      end case;
+      else
+      case op is
+        when "001000" => sal_dummy<="0010";
+        when "000100" => sal_dummy<="0110";
+        when "000101" => sal_dummy<="0110";
+        when "000011" => sal_dummy<="1000";
+        when others => sal_dummy<="0000";
+      end case;
       end if;
     end process;
 end architecture;
