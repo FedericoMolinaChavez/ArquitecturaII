b678fbfa2a2bd6109b054263d2e6761082b92710
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-11 22:57:32 -0500

    cambio out por buffer

--- MUXaddpc.vhd
+++ MUXaddpc.vhd
@@ -1,17 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
 entity MUXaddpc is
   port(
   ent1: in STD_LOGIC_VECTOR (31 downto 0);
   ent2: in STD_LOGIC_VECTOR (31 downto 0);
   ReslB: in std_logic;
-  sal: out STD_LOGIC_VECTOR (31 downto 0)
+  sal: buffer STD_LOGIC_VECTOR (31 downto 0)
   );
 end MUXaddpc;
 
 architecture MUXaddpc_arch of MUXaddpc is
     begin
+    process(ent1,ent2,ReslB,sal)
+    begin
       case ReslB is
-        when "1" => sal<=ent1;
-        when "0" => sal<=ent2;
+        when '1' => sal<=ent1;
+        when '0' => sal<=ent2;
         when others => sal<=ent1;
       end case; 
+    end process;
 end MUXaddpc_arch;
