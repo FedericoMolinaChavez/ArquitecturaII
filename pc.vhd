95434510d903f07f08e2d60ba443925ff18b7c4e
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-13 16:39:14 -0500

    IO logic

--- pc.vhd
+++ pc.vhd
@@ -4,18 +4,23 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity pc is
 	port(
 	clk: in std_logic;
+	IO: in std_logic;
 	ent: in STD_LOGIC_VECTOR (31 downto 0);
 	sal: out STD_LOGIC_VECTOR (31 downto 0));
 end pc;
 
 architecture pc_arch of pc is
 begin
-	process(clk)	
+	process(clk,IO)	
 	begin
-	if (rising_edge(clk)) then
-		sal <= ent;
+	if (IO = '1') then
+		if (rising_edge(clk)) then
+			sal <= ent;
+		else
+			sal <= "00000000000000000000000000000000";
+		end if;
 	else
-	sal <= "00000000000000000000000000000000";
+			
 	end if;
 end process;
 end pc_arch;
