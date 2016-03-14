18dcae7e1e224382f8ae65f384a3bdf26ef6fcc6
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-13 23:45:14 -0500

    Update SyscallROM.vhd

--- SyscallROM.vhd
+++ SyscallROM.vhd
@@ -3,11 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SyscallROM is
 	port(
-		syscall: in std_logic;
-		Entrada: in STD_LOGIC_VECTOR (31 downto 0);
-		DatosS: in STD_LOGIC_VECTOR (31 downto 0);
-		salida: out STD_LOGIC_VECTOR (31 downto 0);
-		Ereg: out STD_LOGIC_VECTOR (31 downto 0)
+		syscall: in std_logic; --señal ent
+		Entrada: in STD_LOGIC_VECTOR (31 downto 0);--de afuera
+		DatosS: in STD_LOGIC_VECTOR (31 downto 0);--de mux memtoreg
+		salida: out STD_LOGIC_VECTOR (31 downto 0); --hacia afuera
+		Ereg: out STD_LOGIC_VECTOR (31 downto 0) --hacia reggistro
 	);
 end SyscallROM;
 
