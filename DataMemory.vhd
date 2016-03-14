e1db999ab83fa3032e8b9f544b791b5dd1a4020a
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-13 20:43:41 -0500

    Update DataMemory.vhd

--- DataMemory.vhd
+++ DataMemory.vhd
@@ -1,5 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.ALL;
 
 entity DataMemory is
 	port(
@@ -12,6 +13,19 @@ entity DataMemory is
 end DataMemory;
 
 architecture DataMemory_arch of DataMemory is
+type dArray is array (127 downto 0) of STD_LOGIC_VECTOR (31 downto 0);
+signal dram: dArray;
+signal data_a: STD_LOGIC_VECTOR (31 downto 0);
 begin
+	data <= data_a;
+	process(entA,entB,memWrite,memRead,data_a)
+	begin
+		case memWrite is
+			when '1' => dram(to_integer(unsigned(entA)))<=entB;
+		end case;
+		case memRead is
+			when '1' => data_a <= dram(to_integer(unsigned(entA)));
+		end case;
+	end process;
 
 end DataMemory_arch;
