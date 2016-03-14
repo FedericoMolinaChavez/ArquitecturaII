7997764da03eff29601589d5d10e916b913edd4d
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-14 14:22:45 -0500

    agregar funcionalidad de boton.

--- SyscallROM.vhd
+++ SyscallROM.vhd
@@ -3,11 +3,12 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity SyscallROM is
 	port(
-		syscall: in std_logic; --señal ent
+		syscall: in std_logic; --señal de unidad de control
+		control: in std_logic; --señal de boton de ingreso de datos.
 		Entrada: in STD_LOGIC_VECTOR (31 downto 0);--de afuera
 		DatosS: in STD_LOGIC_VECTOR (31 downto 0);--de mux memtoreg
 		salida: out STD_LOGIC_VECTOR (31 downto 0); --hacia afuera
-		Ereg: out STD_LOGIC_VECTOR (31 downto 0) --hacia reggistro
+		Ereg: out STD_LOGIC_VECTOR (31 downto 0) --hacia registro
 	);
 end SyscallROM;
 
@@ -19,12 +20,16 @@ architecture SyscallROM_arch of SyscallRom is
 		Ereg<=pseudoEreg;
 		process(syscall,Entrada,DatosS,pseudoSalida,pseudoEreg)
 			begin
+			if (control <= '1') then
 				case Syscall is
 					when '0'=> 
 								pseudoEreg<=Entrada;
 					when '1'=>
 								pseudoSalida<=DatosS;
 				end case;
+			else 
+			
+			end if;
 			end process;
 
 
