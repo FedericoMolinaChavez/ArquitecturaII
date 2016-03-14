bf3ed9a30523a1ec2d45bfb3cc20525f400cd04a
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-13 23:27:13 -0500

    Update CA.vhd

--- CA.vhd
+++ CA.vhd
@@ -3,10 +3,10 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity CA is
   port(
-  op: in STD_LOGIC_VECTOR (5 downto 0);
-  funct: in STD_LOGIC_VECTOR (5 downto 0);
-  AluOp: in STD_LOGIC_VECTOR (2 downto 0);
-  sal: out STD_LOGIC_VECTOR (3 downto 0)
+  op: in STD_LOGIC_VECTOR (5 downto 0); --valor de 6 bits de op
+  funct: in STD_LOGIC_VECTOR (5 downto 0); -- valor de 6 bits del campo de funct
+  AluOp: in STD_LOGIC_VECTOR (2 downto 0); -- Aluop de la unidad de control
+  sal: out STD_LOGIC_VECTOR (3 downto 0) -- funcion de ALU que va a la ALU
   );
 end CA;
 
