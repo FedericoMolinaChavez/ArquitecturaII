359679a806158ef9009d46623c688f36a8e76807
Federico Molina Chavez <federicomolinachavez@gmail.com>
2016-03-18 13:03:09 -0500

    arreglo de CA

--- CA.vhd
+++ CA.vhd
@@ -1,38 +1,42 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity CA is
+entity pruebaHpta is
   port(
   op: in STD_LOGIC_VECTOR (5 downto 0); --valor de 6 bits de op
   funct: in STD_LOGIC_VECTOR (5 downto 0); -- valor de 6 bits del campo de funct
   AluOp: in STD_LOGIC_VECTOR (2 downto 0); -- Aluop de la unidad de control
   sal: out STD_LOGIC_VECTOR (3 downto 0) -- funcion de ALU que va a la ALU
   );
-end CA;
+end pruebaHpta;
 
-architecture CA_arch of CA is
+architecture CA_arch of pruebaHpta is
   signal sal_dummy: STD_LOGIC_VECTOR (3 downto 0);
 begin
   sal <= sal_dummy;
   process(op,funct,AluOp,sal_dummy) 
     begin
-      if (op = "000000") then --Si es tipo R
-      case funct is
-        when "100000" => sal_dummy<="0010";
-        when "100010" => sal_dummy<="0110";
-        when "011010" => sal_dummy<="1110";
-        when "101010" => sal_dummy<="0111";
-        when "010000" => sal_dummy<="1111";
-        when others => sal_dummy<="0000";
+      case AluOp is
+        when "010" =>
+          if (funct = "100010") then
+            sal_dummy <= "0110";
+          elsif (funct = "011010") then
+            sal_dummy <= "1110";
+          elsif (funct = "101010") then
+            sal_dummy <= "0111";
+          else 
+            sal_dummy <= "1111";
+          end if;
+        when "000" =>
+          sal_dummy <= "0010";
+        when "001" =>
+          sal_dummy <="0110";
+        when "111" =>
+          sal_dummy <= "1111";
+       when others =>
+       sal_dummy <= "1111";
       end case;
-      else
-      case op is
-        when "001000" => sal_dummy<="0010";
-        when "000100" => sal_dummy<="0110";
-        when "000101" => sal_dummy<="0110";
-        when "000011" => sal_dummy<="1000";
-        when others => sal_dummy<="0000";
-      end case;
-      end if;
+   
     end process;
 end architecture;
+
