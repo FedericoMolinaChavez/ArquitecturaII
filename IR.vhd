2cc636423df421f5ce86160e24ea50fefb7f192b
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-03-18 22:26:41 -0500

    Create IR.vhd

---
+++ IR.vhd
@@ -0,0 +1,13 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity IR is 
+  port (
+    entrada: in std_logic_vector (31 downto 0); --pilla el relajo que es esto :D
+    salida: out std_logic_vecctor (31 downto 0);
+  );
+end IR;
+
+architecture IR_arch of IR is
+  salida <= entrada;
+end IR_arch;
