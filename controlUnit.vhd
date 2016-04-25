8c75d6e9b417a48e49b5972b46ce9af349c33217
FedericoMolinaChavez <federicomolinachavez@gmail.com>
2016-04-25 18:09:58 -0500

    Update controlUnit.vhd

--- controlUnit.vhd
+++ controlUnit.vhd
@@ -12,7 +12,7 @@ entity uc is
 end uc;
 
 architecture CU_arch of uc is
-type STATE_TYPE is (st_Reset,st_1,st_2,st_3,st_4,st_5,st_6,st_7,st_8,st_9,st_10,st_11,st_12);
+type STATE_TYPE is (st_Reset,st_1,st_2,st_3,st_4,st_5,st_6,st_7,st_8,st_9,st_10,st_11,st_12,st_13,st_14,st_15);
 signal state: state_type;
 
 signal datapath_dummy: STD_LOGIC_VECTOR(19 downto 0);
@@ -29,23 +29,27 @@ begin
 				when st_1 =>
 					state<= st_2; --decode
 				when st_2 =>
-					if(op = "000000") then
+					if(op = "0000") then
 						state <= st_3; --execute
-					elsif (op = "000100")then
+					elsif (op = "1000")then
 						state <= st_6; --branch completion.
-					elsif (op = "001000") then
+					elsif (op = "0100") then
 						state <= st_7; --addi
-					elsif (op = "111111") then
+					elsif (op = "1111") then
 						state <= st_9; --syscalle
-					elsif (op = "111110") then
+					elsif (op = "1110") then
 						state <= st_10; --syscalls
-					elsif (op = "000011") then
+					elsif (op = "0111") then
 						state <= st_12; -- jal
+					elsif (op = "0011") 
+						state <= st_13; --lw
+					elsif (op = "1100")
+						stat <= st_14; --sw
 					else	
 						state <= st_Reset;
 					end if;
 				when st_3 => 
-					if(funct = "001000") then
+					if(funct = "11") then
 						state<=st_5; --completacion jr.
 					else
 						state <= st_4; --completacion tipo r
@@ -71,7 +75,13 @@ begin
 				when st_11 =>
 						state <=st_1; -- retorno a fetch
 				when st_12 =>
-						state <=st_1;
+						state <=st_1; --retorno a fetch
+				when st_13 =>
+						state <= st_15; --writeback
+				when st_14=>
+						state<=st_1; -- retorno a fetch
+				when st_15 =>
+						state<=st_1;
 				when others =>
 				state <= st_Reset; -- cuando llega algo raro se resetea
 				end case;
@@ -84,7 +94,7 @@ begin
 					when st_Reset =>
 						datapath_dummy <= "00000000000000000000";
 					when st_1 =>
-						datapath_dummy <= "00000100000100100000";
+						datapath_dummy <= "00000100000100100000"; 
 					when st_2 =>
 						datapath_dummy <= "00001100000000000000";
 					when st_3 =>
@@ -107,8 +117,12 @@ begin
 						datapath_dummy <= "00000000000000000001";
 					when st_12 =>
 						datapath_dummy <= "00000001001000010100";
-						
-						
+					when st_13 =>
+						datapath_dummy <=
+					when st_14 =>
+						datapath_dummy <=
+					when st_15 =>	
+						datapath_dummy <=
 					when others =>
 						datapath_dummy <= "00000000000000000000";
 				end case;
@@ -116,4 +130,3 @@ begin
 	
 
 end CU_arch;
-
